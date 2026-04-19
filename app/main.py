b2612d40da74727f3f5932de7d196b80943b3716
Per Olsen <persteen22@gmail.com>
2026-04-19 20:09:42 +0200

    Adjusted the root api endpoint

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(history.router)
 
 @app.get("/")
 def root():
-    return {"message": "FastAPI + JWT + ML is running"}
+    return {"message": "FastAPI + JWT + ML v3 is running"}
