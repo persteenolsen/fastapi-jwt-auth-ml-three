c2a00be8fd159e19ad6daa4c4b035c01bc9b20d9
Per Olsen <persteen22@gmail.com>
2026-04-19 19:04:33 +0200

    Added description to the main.py file

--- app/main.py
+++ app/main.py
@@ -5,7 +5,20 @@ from app.routers import auth, predict, history
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="FastAPI ML v3")
+# app = FastAPI(title="FastAPI ML v3")
+
+# -----------------------------
+# INIT APP
+# -----------------------------
+app = FastAPI(
+    title="FastAPI + JWT + ML (v3)",
+    description="19-04-2026 - House Price Prediction API with ML pipeline + JWT auth + PostgreSQL",
+    version="3.0.0",
+    contact={
+        "name": "Per Olsen",
+        "url": "https://persteenolsen.netlify.app",
+    },
+)
 
 app.include_router(auth.router)
 app.include_router(predict.router)
